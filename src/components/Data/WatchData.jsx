2417b5c4a8a4cce8afec59eeeb42095278417a3b
Husayin Bahramov <husayinbahramov@gmial.com>
2025-02-07 14:08:05 +0500

    edit ref

--- src/components/Data/WatchData.jsx
+++ src/components/Data/WatchData.jsx
@@ -15,7 +15,7 @@ export const PopulorWatch = [
     logo: tissotLogo,
     logoWidth: "w-[90px]",
     title: "PR516 CHRONOGRAPH",
-    rafcode: "T-0000000000",
+    rafcode: "T-1494171104100",
     brend: "Tissot",
     price: "520",
     mainImage:
